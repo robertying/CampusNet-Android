74ee8df707dddb5bc0fea5801ad6c03b9db8d1c1
Robert Ying <yingrui205@gmail.com>
2019-03-10 01:50:03 +0800

    Add usereg jni support

--- app/src/main/cpp/jni/Android.mk
+++ app/src/main/cpp/jni/Android.mk
@@ -1,6 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := tidy
+LOCAL_SRC_FILES := ../../../../../htmltidy/.externalNativeBuild/cmake/debug/$(TARGET_ARCH_ABI)/libtidys.a
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/../tunet-c/tidy-html5/include
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := ssl
 LOCAL_SRC_FILES := ../openssl-curl-android/build/openssl/$(TARGET_ARCH_ABI)/lib/libssl.a
 LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/../openssl-curl-android/build/openssl/$(TARGET_ARCH_ABI)/include
@@ -19,9 +25,9 @@ LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/../openssl-curl-android/build/curl/$(TARG
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../openssl/include $(LOCAL_PATH)/../curl/include $(LOCAL_PATH)/../tunet-c/cJSON $(LOCAL_PATH)/../tunet-c/sds $(LOCAL_PATH)/../tunet-c/utf8proc
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../openssl/include $(LOCAL_PATH)/../curl/include $(LOCAL_PATH)/../tunet-c/cJSON $(LOCAL_PATH)/../tunet-c/sds $(LOCAL_PATH)/../tunet-c/utf8proc -I$(LOCAL_PATH)/../tunet-c/tidy-html5/include
 LOCAL_MODULE := tunet
-LOCAL_SRC_FILES := tunet-jni.c ../tunet-c/src/tunet.c ../tunet-c/lib/portal.c ../tunet-c/lib/list.c ../tunet-c/lib/utf.c ../tunet-c/utf8proc/utf8proc.c ../tunet-c/sds/sds.c ../tunet-c/cJSON/cJSON.c
-LOCAL_STATIC_LIBRARIES := libcurl libssl libcrypto
+LOCAL_SRC_FILES := tunet-jni.c ../tunet-c/src/tunet.c ../tunet-c/lib/portal.c ../tunet-c/lib/parser.c ../tunet-c/lib/list.c ../tunet-c/lib/utf.c ../tunet-c/utf8proc/utf8proc.c ../tunet-c/sds/sds.c ../tunet-c/cJSON/cJSON.c
+LOCAL_STATIC_LIBRARIES := libcurl libssl libcrypto libtidy
 LOCAL_LDLIBS := -lz
 include $(BUILD_SHARED_LIBRARY)
--- app/src/main/cpp/jni/tunet-jni.c
+++ app/src/main/cpp/jni/tunet-jni.c
@@ -7,30 +7,28 @@ char *CA_BUNDLE_PATH;
 
 JNIEXPORT void JNICALL Java_io_robertying_campusnet_TunetHelper_tunetInit(JNIEnv *env,
                                                                           jobject this,
-                                                                          jstring path)
-{
+                                                                          jstring path) {
     tunet_init();
-    CA_BUNDLE_PATH = (*env)->GetStringUTFChars(env, path, NULL);
+    CA_BUNDLE_PATH = (char *) (*env)->GetStringUTFChars(env, path, NULL);
 }
 
 JNIEXPORT void JNICALL Java_io_robertying_campusnet_TunetHelper_tunetCleanup(JNIEnv *env,
-                                                                             jobject this)
-{
+                                                                             jobject this) {
     tunet_cleanup();
 }
 
-static jobject enum_c_to_java(JNIEnv *env, res c_enum)
-{
-    jclass response_type = (*env)->FindClass(env, "io/robertying/campusnet/TunetHelper$ResponseType");
-    jfieldID response_field_id = (*env)->GetStaticFieldID(env, response_type, res_string[c_enum], "Lio/robertying/campusnet/TunetHelper$ResponseType;");
+static jobject enum_c_to_java(JNIEnv *env, res c_enum) {
+    jclass response_type = (*env)->FindClass(env,
+                                             "io/robertying/campusnet/TunetHelper$ResponseType");
+    jfieldID response_field_id = (*env)->GetStaticFieldID(env, response_type, res_string[c_enum],
+                                                          "Lio/robertying/campusnet/TunetHelper$ResponseType;");
     return (*env)->GetStaticObjectField(env, response_type, response_field_id);
 }
 
 JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_netLogin(JNIEnv *env,
                                                                             jobject this,
                                                                             jstring username,
-                                                                            jstring password)
-{
+                                                                            jstring password) {
     const char *c_username = (*env)->GetStringUTFChars(env, username, NULL);
     const char *c_password = (*env)->GetStringUTFChars(env, password, NULL);
 
@@ -41,8 +39,7 @@ JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_netLogin(JNIE
 JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_auth4Login(JNIEnv *env,
                                                                               jobject this,
                                                                               jstring username,
-                                                                              jstring password)
-{
+                                                                              jstring password) {
     const char *c_username = (*env)->GetStringUTFChars(env, username, NULL);
     const char *c_password = (*env)->GetStringUTFChars(env, password, NULL);
 
@@ -53,8 +50,7 @@ JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_auth4Login(JN
 JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_auth6Login(JNIEnv *env,
                                                                               jobject this,
                                                                               jstring username,
-                                                                              jstring password)
-{
+                                                                              jstring password) {
     const char *c_username = (*env)->GetStringUTFChars(env, username, NULL);
     const char *c_password = (*env)->GetStringUTFChars(env, password, NULL);
 
@@ -63,22 +59,61 @@ JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_auth6Login(JN
 }
 
 JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_netLogout(JNIEnv *env,
-                                                                             jobject this)
-{
+                                                                             jobject this) {
     res response = net_logout();
     return enum_c_to_java(env, response);
 }
 
 JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_auth4Logout(JNIEnv *env,
-                                                                               jobject this)
-{
+                                                                               jobject this) {
     res response = auth4_logout();
     return enum_c_to_java(env, response);
 }
 
 JNIEXPORT jobject JNICALL Java_io_robertying_campusnet_TunetHelper_auth6Logout(JNIEnv *env,
-                                                                               jobject this)
-{
+                                                                               jobject this) {
     res response = auth6_logout();
     return enum_c_to_java(env, response);
+}
+
+JNIEXPORT jstring JNICALL Java_io_robertying_campusnet_UseregHelper_getSessions(JNIEnv *env,
+                                                                                jobject this,
+                                                                                jstring username,
+                                                                                jstring password) {
+    const char *c_username = (*env)->GetStringUTFChars(env, username, NULL);
+    const char *c_password = (*env)->GetStringUTFChars(env, password, NULL);
+
+    char *result = usereg_get_sessions(c_username, c_password);
+    return (*env)->NewStringUTF(env, result);
+}
+
+JNIEXPORT void JNICALL Java_io_robertying_campusnet_UseregHelper_dropSession(JNIEnv *env,
+                                                                             jobject this,
+                                                                             jstring username,
+                                                                             jstring password,
+                                                                             jstring sessionId) {
+    const char *c_username = (*env)->GetStringUTFChars(env, username, NULL);
+    const char *c_password = (*env)->GetStringUTFChars(env, password, NULL);
+    const char *session_id = (*env)->GetStringUTFChars(env, sessionId, NULL);
+
+    usereg_drop_session(c_username, c_password, session_id);
+}
+
+JNIEXPORT double JNICALL Java_io_robertying_campusnet_UseregHelper_getUsage(JNIEnv *env,
+                                                                            jobject this) {
+    return usereg_get_usage();
+}
+
+JNIEXPORT double JNICALL Java_io_robertying_campusnet_UseregHelper_getUsageDetail(JNIEnv *env,
+                                                                                  jobject this,
+                                                                                  jstring username,
+                                                                                  jstring password,
+                                                                                  jstring startTime,
+                                                                                  jstring endTime) {
+    const char *c_username = (*env)->GetStringUTFChars(env, username, NULL);
+    const char *c_password = (*env)->GetStringUTFChars(env, password, NULL);
+    const char *start_time = (*env)->GetStringUTFChars(env, startTime, NULL);
+    const char *end_time = (*env)->GetStringUTFChars(env, endTime, NULL);
+
+    return usereg_get_usage_detail(c_username, c_password, start_time, end_time);
 }
