6a4b350e164464ee6d0673e0b537922ed4b95947
Robert Ying <yingrui205@gmail.com>
2019-03-23 08:35:17 +0800

    Fix keystore path

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 pushd app/src/main/cpp/openssl-curl-android
 curl -OL "$(curl -s https://api.github.com/repos/robertying/openssl-curl-android/releases/latest | grep browser_download_url | cut -d '"' -f 4)"
-tar xvf build.tar.gz
+tar zxf build.tar.gz
 popd
 bundle exec fastlane beta
