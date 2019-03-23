17ec1483347739dc246d578d04208c7d7f8db896
Robert Ying <yingrui205@gmail.com>
2019-03-23 10:16:05 +0800

    Suppress build output

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd app/src/main/cpp/openssl-curl-android
-curl -OL "$(curl -s https://api.github.com/repos/robertying/openssl-curl-android/releases/latest | grep browser_download_url | cut -d '"' -f 4)"
+curl -sOL "$(curl -s https://api.github.com/repos/robertying/openssl-curl-android/releases/latest | grep browser_download_url | cut -d '"' -f 4)"
 tar zxf build.tar.gz
 popd
 bundle exec fastlane beta
