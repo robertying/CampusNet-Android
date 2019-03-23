35733027d90ae0c3ab2b745b61042291a7a190c9
Robert Ying <yingrui205@gmail.com>
2019-03-23 10:46:07 +0800

    Fix tar args

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 pushd app/src/main/cpp/openssl-curl-android
 curl -sOL "$(curl -s https://api.github.com/repos/robertying/openssl-curl-android/releases/latest | grep browser_download_url | cut -d '"' -f 4)"
-tar zxf build.tar.gz
+tar xf build.tar.gz
 popd
 bundle exec fastlane beta
