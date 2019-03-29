343096592fdc65e175206cd8b8d984066bc65d17
Robert Ying <yingrui205@gmail.com>
2019-03-29 17:01:30 +0800

    Use static filename

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd app/src/main/cpp/openssl-curl-android
-curl -OL "$(curl -s https://api.github.com/repos/robertying/openssl-curl-android/releases/latest | grep browser_download_url | cut -d '"' -f 4)" || exit 1
+curl -L "$(curl -s https://api.github.com/repos/robertying/openssl-curl-android/releases/latest | grep browser_download_url | cut -d '"' -f 4)" -o build.tar.gz || exit 1
 tar xf build.tar.gz
 popd
 bundle exec fastlane beta
